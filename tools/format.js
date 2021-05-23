210bb6af6b98045d146bb7e34a19998f9502d26f
danielrama7 <danielrama017@gmail.com>
2021-05-23 11:08:46 +0700

    update dataset

--- tools/format.js
+++ tools/format.js
@@ -4,6 +4,7 @@ module.exports = fn = data => {
         "nama": data.nama ? data.nama.value : '',
         "kategori": data.kategori ? data.kategori.value : '',
         "nama_ilmiah": data.nama_ilmiah ? data.nama_ilmiah.value : '',
-        "urlFoto": data.urlFoto ? data.urlFoto.value : ''
+        "urlFoto": data.urlFoto ? data.urlFoto.value : '',
+        "deskripsi": data.deskripsi ? data.deskripsi.value : ''
     }
 }
